6aad4b45999da5f2dece83207a557f099eee0d75
Murad1893 <muradmansoor189@gmail.com>
2021-02-23 10:49:54 +0500

    Add project files.

---
+++ MyFirstApplication/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstApplication
+{
+    class Program
+    {
+
+        static string func(string name) { return "Hello " + name; }
+
+        static void Main(string[] args)
+        {
+            // Ctrl + F5 shortcut to run, only F5 to debug
+
+            // Strings
+
+            //Reversing a string
+            string s = "Murad";
+            char[] c = s.ToCharArray();
+            Array.Reverse(c);
+            Console.WriteLine(new string(c));
+
+            // strings also support indexing
+            // taking out substring
+            Console.WriteLine(s.Substring(1,3));
+
+            // divide operation
+            Console.WriteLine(5/2);
+            Console.WriteLine(5/2.0);
+
+            // increment operators ++ and -- supported same as java, c
+            int i = 9;
+            Console.WriteLine(i++ + " " + ++i);
+
+            // Using math library
+            Console.WriteLine(Math.Abs(-40));
+            Console.WriteLine(Math.Pow(3,2)); // 3 ^ 2
+            Console.WriteLine(Math.Sqrt(36));
+            Console.WriteLine(Math.Max(4,90)); // supports only 2 nums. Same goes for Math.Min()
+            Console.WriteLine(Math.Round(4.6) + " " + Math.Round(4.5));
+
+            // Taking user input
+            Console.Write("\nEnter something: "); // we used write instead of writeline
+            string input = Console.ReadLine();
+            Console.WriteLine("Input: " + input);
+
+            Console.WriteLine("Func output: " + func(input));
+
+            // 2d arrays
+            int[,] arr = new int[2, 2];
+            arr[0, 0] = 1;
+            arr[0, 1] = 2;
+            arr[1, 0] = 3;
+            arr[1, 1] = 4;
+
+            Console.WriteLine("\nLength of arr: " + arr.Length);
+            Console.WriteLine("\nDimensions of arr: " + arr.GetLength(0) + ", " + arr.GetLength(1));
+            
+            // special function instead of arr.Length we use arr.GetLength
+            for (i = 0; i < arr.GetLength(0); i++) {
+                for (int j = 0; j < arr.GetLength(1); j++) {
+                    Console.Write(arr[i, j] + " ");
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
---
+++ SampleCalculator/Calculator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleCalculator
+{
+    class Calculator
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Choose from operations: 1.Add 2. Sub 3.Mult 4.Div \nOption: ");
+            int choice = 0;
+            try {
+                choice = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.Message);
+                Environment.Exit(1); // used to sys.exit()
+            }
+            double[] num = new double[2];
+            try
+            {
+                for (int i = 1; i <= 2; i++)
+                {
+                    Console.Write("Enter num" + i + ": ");
+                    num[i - 1] = Convert.ToDouble(Console.ReadLine());
+                }
+            }
+            catch (Exception e){
+                Console.WriteLine("Error: ", e.Message);
+                Environment.Exit(1); // used to sys.exit()
+            }
+            // To display type of variable
+            //Console.WriteLine(num[0].GetType());
+
+            // Included limiting decimal places
+           
+            if (choice == 1)
+            {
+                Console.WriteLine("Answer: " + Math.Round(num[0] + num[1], 3));
+            }
+            else if (choice == 2)
+            {
+                Console.WriteLine("Answer: " + Math.Round(num[0] - num[1], 3));
+            }
+            else if (choice == 3)
+            {
+                Console.WriteLine("Answer: " + Math.Round(num[0] * num[1], 3));
+            }
+            else if (choice == 4)
+            {
+                Console.WriteLine("Answer: " + Math.Round(num[0] / num[1], 3));
+            }
+            else {
+                Console.WriteLine("Incorrect option");
+            }
+        }
+    }
+}
