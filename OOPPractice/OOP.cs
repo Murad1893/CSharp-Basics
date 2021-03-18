2e56ba89c31b390386dbbf492af87139e149a096
Murad1893 <muradmansoor189@gmail.com>
2021-03-18 19:19:25 +0500

    Implementing interfaces

--- OOPPractice/OOP.cs
+++ OOPPractice/OOP.cs
@@ -6,6 +6,34 @@ using System.Threading.Tasks;
 
 namespace OOPPractice
 {
+    interface TesterClass {
+        int Name
+        {
+            get;
+            set;
+        }
+
+    }
+
+    interface TesterClass1
+    {
+
+    }
+
+    class ChildClass: TesterClass, TesterClass1{
+        int a;
+        public ChildClass() : base() {
+            a = 2;
+        }
+
+        public int Name
+        {
+            get { return this.a; }
+            set { this.a = value; }
+        }
+
+    }
+
     class Anything
     {
         static void Main(string[] args)
